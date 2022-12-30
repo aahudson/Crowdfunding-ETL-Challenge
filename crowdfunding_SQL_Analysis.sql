569130d223a59941eb771d7306448842ff344bff
aahudson <aahudson@liberty.edu>
2022-12-30 14:06:18 -0500

    created email_backers_remaining_goal_amount table

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -43,15 +43,21 @@ SELECT * FROM email_contacts_remaining_goal_amount;
 
 --  Export the table as email_backers_remaining_goal_amount.csv
 
-COPY (SELECT * FROM email_contacts_remaining_goal_amount) TO '/path/to/email_backers_remaining_goal_amount.csv' WITH (FORMAT CSV, HEADER TRUE);
-
 -- 4. (5 pts)
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
 
+SELECT  b.email, b.first_name, b.last_name, b.cf_id, ca.company_name, ca.description, ca.end_date, ca.goal - ca.pledged AS "Left_of_Goal" 
+INTO email_backers_remaining_goal_amount
+FROM backers AS b
+JOIN campaign AS ca
+ON ca.cf_id = b.cf_id
+WHERE ca.outcome = 'live'
+ORDER BY "email" DESC;
 
 
 -- Check the table
 
+SELECT * FROM email_backers_remaining_goal_amount; 
 
